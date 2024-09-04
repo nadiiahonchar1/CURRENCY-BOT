c7c5e79b6f10949ef071e69169fd3530640253a6
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-09-04 16:41:38 +0300

    added bot.start

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const Telegraf = require('telegraf')
+const Telegraf = require("telegraf");
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  return ctx.reply("Welcome to Mono Currency Bot");
+});
+bot.startPolling();
