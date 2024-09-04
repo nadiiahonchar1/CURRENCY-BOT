81a97de2579ac42221937fb8ad128c7027328e0f
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-09-04 17:26:06 +0300

    add get currency

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const Telegraf = require("telegraf");
 require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const  axios  = require("axios");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
   return ctx.reply("Welcome to Mono Currency Bot");
 });
+
+bot.hears('hi', (ctx) => {
+    axios.get("https://api.monobank.ua/bank/currency").then((res) => {
+        console.log(res.data[0]);
+        return ctx.reply(res.data[0])
+    }).catch((err) => {
+        return ctx.reply(err);
+    });
+})
 bot.startPolling();
