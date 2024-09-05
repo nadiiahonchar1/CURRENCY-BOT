5dda3e6a5c5cbd65af0fe98767b6eeb4f9b66b3c
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-09-05 14:02:59 +0300

    added filter

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
-const  axios  = require("axios");
+const axios = require("axios");
+const cc = require("currency-codes");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  return ctx.reply("Welcome to Mono Currency Bot");
+  return ctx.reply("Ласкаво просимо у  Mono Currency Bot");
 });
 
-bot.hears('hi', (ctx) => {
-    axios.get("https://api.monobank.ua/bank/currency").then((res) => {
-        console.log(res.data[0]);
-        return ctx.reply(res.data[0])
-    }).catch((err) => {
-        return ctx.reply(err);
+bot.hears(/^[A-Z]+$/i, async (ctx) => {
+  const clientCurrCode = ctx.message.text;
+  const currency = cc.code(clientCurrCode);
+  if (!currency) {
+    return ctx.reply("Така валюта не знайдена");
+  }
+  try {
+    const currencyObj = await axios.get(
+      "https://api.monobank.ua/bank/currency"
+    );
+    const foundCurrency = currencyObj.data.find((cur) => {
+      return cur.currencyCodeA.toString() === currency.number;
     });
-})
+    if (!foundCurrency || !foundCurrency.rateBuy || !foundCurrency.rateSell) {
+      return ctx.reply("Така валюта не знайдена");
+    }
+    // return ctx.reply(JSON.stringify(foundCurrency));
+    return ctx.replyWithMarkdown(`
+ВАЛЮТА:   *${currency.code}*
+ПОКУПКА:  *${foundCurrency.rateBuy} грн*
+ПРОДАЖ:   *${foundCurrency.rateSell} грн*
+          `);
+  } catch (error) {
+    return ctx.reply(error);
+  }
+});
 bot.startPolling();
